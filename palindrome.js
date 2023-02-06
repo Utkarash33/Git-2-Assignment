600e53f0fce5e62dc14b3c2b0e14a9d9d371404f
Utkarash33 <atulthakur@gmail.com>
2023-02-06 23:51:51 +0530

    assignment is done

---
+++ palindrome.js
@@ -0,0 +1,11 @@
+let str=1221;
+let stri="";
+for(let i=str.length-1;i>=0;i-){
+  stri=stri+str[i]
+} 
+if(stri=str)
+{
+    console.log("Yes")
+} else {
+    console.log("No")
+}
---
+++ prime.js
@@ -0,0 +1,15 @@
+let num=1;
+let count=0;
+for (let i=1;i<=num;i+)
+  {
+    if ((num%i)==0)
+    {
+      count++;
+    }
+  } if (count>2 && count<2)
+{
+  console.log("No")
+}  else if (count==2)
+{
+  console.log("Yes")
+} 
